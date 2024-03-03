fa97c20390f967a0df0f511a694ac5c74637c6f2
Tasha Miller <popelyshkon25@gmail.com>
2024-03-03 18:06:41 +0200

    Created POM for login page

---
+++ app/login.page.ts
@@ -0,0 +1,18 @@
+import { Page } from "@playwright/test";
+
+export const login = (page: Page) => {
+    return {
+        async open() {
+            await page.goto('https://shopdemo-alex-hot.koyeb.app/');
+        },
+
+        async doLogin(email: string, password: string) {
+            await this.open(page);
+            await page.getByRole('link', { name: 'Welcome!' }).click();
+            await page.getByRole('menuitem', { name: 'Login' }).click();
+            await page.getByRole('main').getByPlaceholder('Please Enter Your Email').fill(email);
+            await page.getByPlaceholder('Please Enter Your Password').fill(password);
+            await page.getByRole('button', { name: 'Login' }).click();
+        }
+    }
+}
--- tests/purchase.test.ts
+++ tests/purchase.test.ts
@@ -1,16 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
+import { login } from '../app/login.page';
+
 
 test('User can buy product', async ({ page }) => {
-    await page.goto('https://shopdemo-alex-hot.koyeb.app/');
-    await page.getByRole('link', { name: 'Welcome!' }).click();
-    await page.getByRole('menuitem', { name: 'Login' }).click();
-    await page.getByRole('main').getByPlaceholder('Please Enter Your Email').fill('xotabu4@gmail.com');
-    await page.getByPlaceholder('Please Enter Your Password').fill('xotabu4@gmail.com');
-    await page.getByRole('button', { name: 'Login' }).click();
+    
+    await login.doLogin(page, 'xotabu4@gmail.com', 'xotabu4@gmail.com');
     await page.getByRole('link', { name: 'Brands ' }).click();
     await page.getByRole('menuitem', { name: 'Nizhyn cannery' }).click();
     await page.getByRole('link', { name: 'CHERRY TOMATOES By Nizhyn' }).click();
     await page.getByRole('button', { name: 'Add To Bag' }).click();
     await page.getByRole('button', { name: 'Place Order' }).click();
-    await expect(page.locator('.order-message').locator('h2')).toContainText('Thank you for your order.');
+    //await expect(page.locator('.order-message').locator('h2')).toContainText('Thank you for your order.');
+    await expect(page.locator('h2', {hasText: 'Thank you for your order.'})).toBeVisible();
 });
