2bf91aed02ae29bd1c87440748616dd9479d0561
Tasha Miller <popelyshkon25@gmail.com>
2024-03-03 12:38:49 +0200

    Removed bad test :D

--- tests/test-3.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('User can google inappropriate stuff', async ({ page }) => {
-  await page.goto('https://www.google.com/search?q=natashatop%40gmail.com&oq=natashatop%40gmail.com&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRg60gEHNzM3ajBqMqgCALACAA&sourceid=chrome&ie=UTF-8');
-  await expect(page.locator('#rso')).toContainText('Female Escorts In Lisbon Portugal');
-});
